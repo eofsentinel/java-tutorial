7471e9a049e9893a0322ab879a9f9b1e6d81f6b6
Abhishek Chakraborty <eofsentinel@outlook.com>
2024-07-27 22:47:12 +0530

    First Java Tutorial and other settings

---
+++ tutorial1/src/main/java/com/tutorial1/Main.java
@@ -0,0 +1,10 @@
+package com.tutorial1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!!");
+        int age = 31;
+        int herAge = age - 3;
+        System.out.println("MyAge: " + age + "\nHerAge: " + herAge);
+    }
+}
