caf2acdadd4a59116c1d693536056514d791f93b
Abhishek Chakraborty <eofsentinel@outlook.com>
2024-08-03 14:04:39 +0530

    Demo Spring Boot Web app

---
+++ demoapp/src/main/java/com/eofsentinel/demoapp/Hello.java
@@ -0,0 +1,12 @@
+package com.eofsentinel.demoapp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+    @RequestMapping("/")
+    public String greet() {
+        return "Hello world! Welcome to Java Spring Boot Tutorial!";
+    }
+}
