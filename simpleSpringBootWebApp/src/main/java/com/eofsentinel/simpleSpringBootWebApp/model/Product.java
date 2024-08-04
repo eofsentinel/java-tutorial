e0627823f1aad9b7a5a35332944858333d3ad016
Abhishek Chakraborty <eofsentinel@outlook.com>
2024-08-04 17:36:44 +0530

    Added storage to Simple Spring Boot Web Application for Product Catalog

--- simpleSpringBootWebApp/src/main/java/com/eofsentinel/simpleSpringBootWebApp/model/Product.java
+++ simpleSpringBootWebApp/src/main/java/com/eofsentinel/simpleSpringBootWebApp/model/Product.java
@@ -1,9 +1,16 @@
 package com.eofsentinel.simpleSpringBootWebApp.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Product {
+    @Id
     private int prodId;
     private String prodName;
     private double price;
+
+    public Product() { }
 
     public Product(int prodId, String prodName, double price) {
         this.prodId = prodId;
--- simpleSpringBootWebApp/src/main/java/com/eofsentinel/simpleSpringBootWebApp/service/ProductService.java
+++ simpleSpringBootWebApp/src/main/java/com/eofsentinel/simpleSpringBootWebApp/service/ProductService.java
@@ -1,6 +1,8 @@
 package com.eofsentinel.simpleSpringBootWebApp.service;
 
 import com.eofsentinel.simpleSpringBootWebApp.model.Product;
+import com.eofsentinel.simpleSpringBootWebApp.repository.IProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,62 +11,85 @@ import java.util.List;
 @Service
 public class ProductService {
 
-    List<Product> products = new ArrayList<Product>() {
-        {
-            add(new Product(101, "iPhone", 999.99));
-            add(new Product(102, "Macbook Air", 1299.90));
-            add(new Product(103, "Macbook Pro 2024", 2499.95));
-        }
-    };
+    @Autowired
+    IProductRepository productRepo;
+
+//    List<Product> products = new ArrayList<Product>() {
+//        {
+//            add(new Product(101, "iPhone", 999.99));
+//            add(new Product(102, "Macbook Air", 1299.90));
+//            add(new Product(103, "Macbook Pro 2024", 2499.95));
+//        }
+//    };
 
     public List<Product> getProducts() {
-        return this.products;
+//        return this.products;
+        return this.productRepo.findAll();
     }
 
     public Product getProductById(int id) {
-        return this.products.stream()
-                .filter(x -> x.getProdId() == id).findFirst()
-                .orElse(new Product(id, "Product not found", 0));
+//        return this.products.stream()
+//                .filter(x -> x.getProdId() == id).findFirst()
+//                .orElse(new Product(id, "Product not found", 0));
+        return this.productRepo.findById(id).orElse(new Product());
     }
 
     public boolean addProduct(Product prod) {
-        if (this.products.stream().anyMatch(x -> x.getProdId() == prod.getProdId())) {
+//        if (this.products.stream().anyMatch(x -> x.getProdId() == prod.getProdId())) {
+//            return false;
+//        }
+//
+//        this.products.add(prod);
+//        return true;
+        if (this.productRepo.existsById(prod.getProdId())) {
             return false;
         }
 
-        this.products.add(prod);
+        this.productRepo.save(prod);
         return true;
     }
 
     public boolean updateProduct(Product prod) {
-        if (this.products.stream().noneMatch(x -> x.getProdId() == prod.getProdId())) {
+//        if (this.products.stream().noneMatch(x -> x.getProdId() == prod.getProdId())) {
+//            return false;
+//        }
+//
+//        for (int i = 0; i < this.products.size(); i++) {
+//            if (this.products.get(i).getProdId() == prod.getProdId()) {
+//                this.products.set(i, prod);
+//                break;
+//            }
+//        }
+//
+//        return true;
+        if (!this.productRepo.existsById(prod.getProdId())) {
             return false;
         }
 
-        for (int i = 0; i < this.products.size(); i++) {
-            if (this.products.get(i).getProdId() == prod.getProdId()) {
-                this.products.set(i, prod);
-                break;
-            }
-        }
-
+        this.productRepo.save(prod);
         return true;
     }
 
     public boolean deleteProduct(int prodId) {
-        int index = -1;
-        for (int i = 0; i < this.products.size(); i++) {
-            if (this.products.get(i).getProdId() == prodId) {
-                index = i;
-                break;
-            }
+//        int index = -1;
+//        for (int i = 0; i < this.products.size(); i++) {
+//            if (this.products.get(i).getProdId() == prodId) {
+//                index = i;
+//                break;
+//            }
+//        }
+//
+//        if (index != -1) {
+//            this.products.remove(index);
+//            return true;
+//        }
+//
+//        return false;
+        if (!this.productRepo.existsById(prodId)) {
+            return false;
         }
 
-        if (index != -1) {
-            this.products.remove(index);
-            return true;
-        }
-
-        return false;
+        this.productRepo.deleteById(prodId);
+        return true;
     }
 }
